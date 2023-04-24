5e03396502c3bc6e87d490a37480889315bc59f1
Yorichitypezero <131659517+Yorichitypezero@users.noreply.github.com>
2023-04-24 15:25:23 +0530

    Update streamlit_app.py
    Another Header creation

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,5 @@ streamlit.text('🥣 Omega 3 and blueberry oatmeal')
 streamlit.text('🥗 Kale,Spinach and Rocket Smoothie')
 streamlit.text('🐔 Hard Boiled Egg Free-Range Egg')
 streamlit.text('🥑🍞 Avocado toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
