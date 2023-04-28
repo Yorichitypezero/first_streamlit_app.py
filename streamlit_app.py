35df22fcd8e1d3f1c379ac7048e82a14d4aa1c3a
Yorichitypezero <131659517+Yorichitypezero@users.noreply.github.com>
2023-04-28 17:18:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,4 +66,4 @@ add_my_fruit = streamlit.text_input('What fruit would you like information about
 streamlit.write('Thanks for adding ', add_my_fruit)
 
 #Will not work correctly
-my_cur.execute("insert into fruit_load_list values('from streamlit))
+my_cur.execute("insert into fruit_load_list values('from streamlit'))
