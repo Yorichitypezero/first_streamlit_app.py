9f12427a3a0ec64fff0cb58986fe9008d8f022fd
Yorichitypezero <131659517+Yorichitypezero@users.noreply.github.com>
2023-04-28 17:24:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 #Importing the library Streamlit
 import streamlit
+import pandas
+import requests
+import snowflake.connector
+import urllib.error import URLError
 
 #Title
 streamlit.title('My Parents Healthy Dinner')
@@ -17,7 +21,7 @@ streamlit.text('🥑🍞 Avocado toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 #Importing Pandas
-import pandas
+#import pandas
 
 #Reading the CSV files from the Amazon AWS S3 bucket
 my_fruit_list=pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
@@ -44,7 +48,7 @@ streamlit.header('Fruityvice Fruit Advice!')
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Apple')
 streamlit.write('The user entered ', fruit_choice)
 
-import requests
+#import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 
 #normalise the json file format
@@ -53,7 +57,9 @@ fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
 #output to be displayed on the screen
 streamlit.dataframe(fruityvice_normalized)
 
-import snowflake.connector
+streamlit.stop()
+
+#import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
