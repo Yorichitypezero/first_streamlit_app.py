5d6f767738bd96871d68ba93bdf47df6018ec954
Yorichitypezero <131659517+Yorichitypezero@users.noreply.github.com>
2023-04-28 17:17:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,3 +64,6 @@ streamlit.dataframe(my_data_row)
 #Second entry box creation
 add_my_fruit = streamlit.text_input('What fruit would you like information about?','Jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit)
+
+#Will not work correctly
+my_cur.execute("insert into fruit_load_list values('from streamlit))
