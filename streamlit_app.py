09027f78d1580af9a22b7c947ae6c199f621c359
Yorichitypezero <131659517+Yorichitypezero@users.noreply.github.com>
2023-04-28 17:45:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,9 +20,6 @@ streamlit.text('🥑🍞 Avocado toast')
 #Another Header
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-#Importing Pandas
-#import pandas
-
 #Reading the CSV files from the Amazon AWS S3 bucket
 my_fruit_list=pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 #streamlit.dataframe(my_fruit_list)
@@ -43,20 +40,18 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 #Displaying the output
 streamlit.dataframe(fruits_to_show)
 
-#Importing Requests to display FruitVice API using User choice
+
 streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Apple')
-streamlit.write('The user entered ', fruit_choice)
-
-#import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-
-#normalise the json file format
-fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
-
-#output to be displayed on the screen
-streamlit.dataframe(fruityvice_normalized)
-
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+    fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+except URELError as e:
+  streamlit.error()
 streamlit.stop()
 
 #import snowflake.connector
